5356ddb5622663160b4cf77f4cf468afb4f35249
sooti <sooti>
2014-11-27 11:01:53 +0100

    d855: select qcwcn mixer_paths.xml
    Change-Id: I85237154b5bbc54dbebb673f55189b1b3d2e9907

--- nameless_d855.mk
+++ nameless_d855.mk
@@ -1,7 +1,6 @@
 $(call inherit-product, device/lge/d855/full_d855.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Enhanced NFC
 $(call inherit-product, vendor/nameless/config/nfc_enhanced.mk)
@@ -13,3 +12,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="g3_global_com" \
     BUILD_FINGERPRINT="lge/g3_global_com/g3:4.4.2/KVT49L.A1404795316/1404795316:user/release-keys" \
     PRIVATE_BUILD_DESC="g3_global_com-user 4.4.2 KVT49L.A1404795316 1404795316 release-keys"
+include vendor/nameless/config/common.mk
+$(call inherit-product, vendor/nameless/config/apns.mk)
+PRODUCT_NAME := nameless_d855
